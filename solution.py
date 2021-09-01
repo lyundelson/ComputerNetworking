9bc1d2355eac2e9c508c13d0bea1a21e0095ee96
lyundelson <89924576+lyundelson@users.noreply.github.com>
2021-09-01 11:52:05 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,17 +8,17 @@ def welcome_assignment_answers(question):
     if question == "In Slack, what is the secret passphrase posted in the #cyberfellows-computernetworking-fall2021 channel posted by a TA?":
         answer = "mTLS"    
     elif question == "Are encoding and encryption the same? - Yes/No":
-        answer = "No"
+        answer = "no"
     elif question == "Is it possible to decrypt a message without a key? - Yes/No":
-        answer = "Yes"
+        answer = "no"
     elif question ==  "Is it possible to decode a message without a key?":
-        answer = "Yes"
+        answer = "yes"
     elif question ==  "Is a hashed message supposed to be un-hashed?":
-        answer = "No"
+        answer = "no"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
     elif question == "Is MD5 a secured hashing algorithm?":
-        answer = "Yes"
+        answer = "yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 5
     elif question == "What layer of the TCP/IP model the protocol TCP belongs to? - The answer should be a numeric number":

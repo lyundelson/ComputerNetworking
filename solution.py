2bba477187121b222d3004693a08cef28c55a97e
lyundelson <89924576+lyundelson@users.noreply.github.com>
2021-09-01 11:57:05 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -17,7 +17,7 @@ def welcome_assignment_answers(question):
         answer = "no"
     elif question == "What is the MD5 hashing value to the following message: 'NYU Computer Networking' - Use MD5 hash generator and use the answer in your code":
         answer = "42b76fe51778764973077a5a94056724"
-    elif question == "Is MD5 a secured hashing algorithm?":
+    elif question == "Is MD5 a secured hashing algorithm? - Yes/No":
         answer = "yes"
     elif question == "What layer from the TCP/IP model the protocol DHCP belongs to? - The answer should be a numeric number":
         answer = 5

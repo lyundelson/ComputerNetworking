85b9fd8f78e066916f5928e239e8b1b8a7bc196f
Larry Yundelson <ly2177@nyu.edu>
2021-09-01 11:19:26 -0400

    test

--- solution.py
+++ solution.py
@@ -17,3 +17,5 @@ if __name__ == "__main__":
     #use this space to debug and verify that the program works
     debug_question = "Are encoding and encryption the same? - Yes/No"
     print(welcome_assignment_answers(debug_question))
+
+    #testing git
